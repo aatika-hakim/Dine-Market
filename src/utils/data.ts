7aa8c94d0875679cc3f8eb51917154840e1c95fe
Aatika hakim <aatikahakim90@gmail.com>
2024-03-11 23:19:29 +0500

    products data

---
+++ src/utils/data.ts
@@ -0,0 +1,136 @@
+
+import P1 from "../../public/assets/images/product-1.png";
+import P2 from "../../public/assets/images/product-2.png";
+import P3 from "../../public/assets/images/product-3.png";
+import P4 from "../../public/assets/images/product-4.png";
+import P5 from "../../public/assets/images/product-5.png";
+import P6 from "../../public/assets/images/product-6.png";
+import P7 from "../../public/assets/images/product-7.png";
+import P8 from "../../public/assets/images/product-8.png";
+import P9 from "../../public/assets/images/product-9.png";
+import P10 from "../../public/assets/images/product-10.png";
+import P11 from "../../public/assets/images/product-11.png";
+
+export const products = [
+    {
+        id: 1,
+        name: "Product 1",
+        tags: "Dress",
+        category: "female",
+        price: 200,
+        image: P1,
+        discount: 2,
+        qty: 1,
+        slug: "p1",
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        tags: "Dress",
+        category: "female",
+        price: 600,
+        image: P2,
+        discount: 2,
+        qty: 1,
+        slug: "p2",
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        tags: "Dress",
+        category: "Male",
+        price: 900,
+        image: P3,
+        discount: 2,
+        qty: 1,
+        slug: "p3",
+    },
+    {
+        id: 4,
+        name: "Product 4",
+        tags: "Dress",
+        category: "female",
+        price: 200,
+        image: P4,
+        discount: 2,
+        qty: 1,
+        slug: "p4",
+    },
+    {
+        id: 5,
+        name: "Product 5",
+        tags: "Dress",
+        category: "female",
+        price: 200,
+        image: P5,
+        discount: 2,
+        qty: 1,
+        slug: "p5",
+    },
+    {
+        id: 6,
+        name: "Product 6",
+        tags: "Dress",
+        category: "female",
+        price: 200,
+        image: P6,
+        discount: 2,
+        qty: 1,
+        slug: "p6",
+    },
+    {
+        id: 7,
+        name: "Product 7",
+        tags: "Dress",
+        category: "kids",
+        price: 890,
+        image: P7,
+        discount: 2,
+        qty: 1,
+        slug: "p7",
+    },
+    {
+        id: 8,
+        name: "Product 8",
+        tags: "Dress",
+        category: "kids",
+        price: 1200,
+        image: P8,
+        discount: 2,
+        qty: 1,
+        slug: "p8",
+    },
+    {
+        id: 9,
+        name: "Product 9",
+        tags: "Dress",
+        category: "female",
+        price: 1200,
+        image: P9,
+        discount: 2,
+        qty: 1,
+        slug: "p9",
+    },
+    {
+        id: 10,
+        name: "Product 10",
+        tags: "Dress",
+        category: "female",
+        price: 1200,
+        image: P10,
+        discount: 2,
+        qty: 1,
+        slug: "p10",
+    },
+    {
+        id: 11,
+        name: "Product 11",
+        tags: "Dress",
+        category: "male",
+        price: 1200,
+        image: P11,
+        discount: 2,
+        qty: 1,
+        slug: "p11",
+    }
+];
